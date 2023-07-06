5ab4adee79962a4e3a174b1a48254101fe80ce7c
blackmax <blackmax1886@gmail.com>
2023-07-06 04:26:22 +0000

    Add: utils for get input from AtCoder

---
+++ contest/utils/input.py
@@ -0,0 +1,28 @@
+def getInt():
+    return int(input())
+
+def getInts():
+    return map(int, input().split())
+
+def getIntsArray():
+    return list(map(int, input().split()))
+
+# get vertical input of ints as array
+def getIntsVArray(N):
+    arr = []
+    for _ in range(N):
+        arr.append(int(input()))
+    return arr
+
+# get vertical input of ints as set
+def getIntsVSet(N):
+    s = set()
+    for _ in range(N):
+        s.add(int(input()))
+    return s
+
+def getIntsMatrix(N):
+    mat = []
+    for _ in range(N):
+        mat.append(list(map(int, input().split())))
+    return mat
