8fc468c9cf1fbb72f2d29dab9cf3141cfa5a3492
blackmax <blackmax1886@gmail.com>
2023-07-27 05:44:11 +0000

    Refactor: add comments

--- contest/utils/graph.py
+++ contest/utils/graph.py
@@ -1,5 +1,12 @@
 from collections import deque
 
+"""
+Parameters:
+graph : dict - グラフを表す辞書。キーは頂点、値はその頂点から出ていくエッジの終点のリスト。
+
+Returns:
+list - トポロジカルソートされたノードのリスト。巡回が存在する場合、空のリストを返します。
+"""
 def topological_sort(graph):
     in_degree = {u: 0 for u in graph}
     for u in graph:
@@ -32,6 +39,21 @@ def topological_sort(graph):
 
 # print(topological_sort(graph))
 
+"""
+この関数は、頂点の数とエッジのリストを与えると、有向グラフを表現する辞書を作成します。
+
+各頂点は1からnode_numまでの整数で表されます。エッジは、(始点, 終点)の形式のタプルかリストで表されます。 
+エッジのリストは、すべてのエッジを含むリストで、エッジの始点と終点は頂点に対応します。
+
+グラフは辞書で表現され、キーは頂点で、値はその頂点から出ていくエッジの終点のリストです。
+
+Parameters:
+node_num : int - グラフの頂点の数
+edges : list - エッジのリスト。各エッジは (始点, 終点)の形式のタプルかリスト
+
+Returns:
+graph : dict - グラフを表す辞書。キーは頂点、値はその頂点から出ていくエッジの終点のリスト
+"""
 def make_directed_graph(node_num : int, edges: list):
     graph = {i: [] for i in range(1, node_num+1)}
     for start, end in edges:
